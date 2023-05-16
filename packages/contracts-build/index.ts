3aafd1a40c1bddc1639320982a0662d6676f989a
Pham Tu <tubackkhoa@gmail.com>
2023-05-16 11:11:29 +0700

    add entry point for contracts-build

---
+++ packages/contracts-build/index.ts
@@ -0,0 +1,27 @@
+import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate';
+import { readFileSync } from 'fs';
+import path from 'path';
+
+const contractDir = path.join(path.dirname(module.filename), 'data');
+
+export type ContractName = 'multicall';
+
+export const getContractDir = (name: ContractName = 'multicall') => {
+  return path.join(contractDir, name + '.wasm');
+};
+
+export const deployContract = async <T>(client: SigningCosmWasmClient, senderAddress: string, msg: T, label: string, contractName?: ContractName) => {
+  // upload and instantiate the contract
+  const wasmBytecode = readFileSync(getContractDir(contractName));
+  const uploadRes = await client.upload(senderAddress, wasmBytecode, 'auto');
+  const initRes = await client.instantiate(senderAddress, uploadRes.codeId, msg, label, 'auto');
+  return { ...uploadRes, ...initRes };
+};
+
+export const migrateContract = async <T>(client: SigningCosmWasmClient, senderAddress: string, contractAddress: string, msg: T, contractName?: ContractName) => {
+  // upload and instantiate the contract
+  const wasmBytecode = readFileSync(getContractDir(contractName));
+  const uploadRes = await client.upload(senderAddress, wasmBytecode, 'auto');
+  const migrateRes = await client.migrate(senderAddress, contractAddress, uploadRes.codeId, msg, 'auto');
+  return { ...uploadRes, ...migrateRes };
+};
