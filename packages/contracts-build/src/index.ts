3426869480dd2499e9b345bcf0065ea08bf3103f
Pham Tu <tubackkhoa@gmail.com>
2023-07-17 14:34:28 +0700

    add cw721-base

--- packages/contracts-build/src/index.ts
+++ packages/contracts-build/src/index.ts
@@ -3,6 +3,7 @@ import {
   Cw1SubkeysTypes,
   Cw1WhitelistTypes,
   Cw20BaseTypes,
+  Cw721BaseTypes,
   Cw20Ics20Types,
   Cw3FixedMultisigTypes,
   Cw3FlexMultisigTypes,
@@ -15,12 +16,13 @@ import {
 import { readFileSync } from 'fs';
 import path from 'path';
 
-export type ContractName = 'multicall' | 'cw-ics20-latest' | 'cw-ics721-bridge' | 'cw1-subkeys' | 'cw1-whitelist' | 'cw20-base' | 'cw20-ics20' | 'cw3-fixed-multisig' | 'cw3-flex-multisig' | 'cw4-group' | 'cw4-stake';
+export type ContractName = 'multicall' | 'cw-ics20-latest' | 'cw-ics721-bridge' | 'cw1-subkeys' | 'cw1-whitelist' | 'cw20-base' | 'cw721-base' | 'cw20-ics20' | 'cw3-fixed-multisig' | 'cw3-flex-multisig' | 'cw4-group' | 'cw4-stake';
 export type InstantiateMsg =
   | CwIcs20LatestTypes.InstantiateMsg
   | Cw1SubkeysTypes.InstantiateMsg
   | Cw1WhitelistTypes.InstantiateMsg
   | Cw20BaseTypes.InstantiateMsg
+  | Cw721BaseTypes.InstantiateMsg
   | Cw20Ics20Types.InstantiateMsg
   | Cw3FixedMultisigTypes.InstantiateMsg
   | Cw3FlexMultisigTypes.InstantiateMsg
